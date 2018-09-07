4b30b806de6c9e2f0340dc057e949e17b1bfb590
Ivan Kravchuk <ikravchuk@softserveinc.com>
2018-09-07 18:53:21 +0300

    innitial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weather:any;
+  // location:{
+    city:string
+  // }
+
+  constructor(public navCtrl: NavController, private wp: WeatherProvider) {
 
   }
 
+  ionViewWillEnter(){
+    // this.location = {
+    //   city: 'Lviv'
+    // }
+
+    // this.wp.getCurrentForecastInHoursByName(this.location.city).subscribe(weather => {
+    //   // console.log(weather.list);
+    //   this.weather = (weather.list as Array<any>).map(item => {
+    //     return { data: item.dt_txt, temp: item.main.temp }
+    //   });
+    //   console.log(this.weather);
+    // })
+
+    this.wp.getCoordinates().then(coordinates => {
+      this.wp.getCurrentForecastInHoursByCoordinates(coordinates).subscribe(weather => {
+        console.log(weather);
+        this.city = weather.city.name;
+        
+        this.weather = (weather.list as Array<any>).map(item => {
+          return { data: item.dt_txt, temp: item.main.temp }
+        });
+      });
+    });;
+
+  }
 }
