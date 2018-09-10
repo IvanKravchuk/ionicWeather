a526031165647e45d9e87d07b31a906456ae7708
Ivan Kravchuk <ikravchuk@softserveinc.com>
2018-09-10 18:13:18 +0300

    add google maps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
 
+// @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  weather:any;
-  city:string;
-  title:string = 'Home';
+  weather: any;
+  city: string;
+  title: string = 'Home';
+  lat: string;
+  lng: string;
 
-  constructor(public navCtrl: NavController, private wp: WeatherProvider, public navParams: NavParams) {
-    this.city = navParams.get('city');
-    this.title = navParams.get('title');
-    console.log(navParams.get('title'));
+  constructor(public navCtrl: NavController, private wp: WeatherProvider, public navParams: NavParams, public events: Events) {
+    // this.city = navParams.get('city');
+    // this.title = navParams.get('title');
+    // console.log(navParams.get('title'));
+    this.events.subscribe('my-message', (data) =>{
+      console.log("data", data);
+      this.city = data.city;
+      this.lat = data.lat;
+      this.lng = data.lng;
+    });
   }
 
   ionViewWillEnter(){
-    if(!this.city){
+    if(!this.city && !this.lat){
       this.wp.getCoordinates().then(coordinates => {
         this.wp.getCurrentForecastInHoursByCoordinates(coordinates).subscribe(weather => {
           this.city = weather.city.name;
@@ -28,12 +37,21 @@ export class HomePage {
           });
         });
       });;
-    } else {
+    } else if(this.city) {
       this.wp.getCurrentForecastInHoursByName(this.city).subscribe(weather => {
         this.weather = (weather.list as Array<any>).map(item => {
           return { data: item.dt_txt, temp: item.main.temp }
         });
       })
+    } else {
+      console.log({ lat: this.lat, lon: this.lng });
+      this.wp.getCurrentForecastInHoursByCoordinates({ lat: this.lat, lon: this.lng }).subscribe(weather => {
+        console.log(weather);
+        this.city = weather.city.name;
+        this.weather = (weather.list as Array<any>).map(item => {
+          return { data: item.dt_txt, temp: item.main.temp }
+        });
+      });
     }
   }
 }
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 import { Keyboard } from '@ionic-native/keyboard';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home';
+import { IonicPage, NavController, NavParams, Events } from 'ionic-angular';
+// import { HomePage } from '../home/home';
 
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+declare var google: any;
+// @IonicPage()
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
@@ -18,27 +12,55 @@ import { HomePage } from '../home/home';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private keyboard: Keyboard) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private keyboard: Keyboard, public events: Events) {
     this.title = 'Settings';
   }
 
   city: string;
   title: string;
+  private map: any;
+  private lat: string;
+  private lng: string;
 
   showForecastByCity(){
     this.keyboard.close();
-    this.navCtrl.push(HomePage, {
-      'city': this.city,
-      'title': this.title
-    });
+    this.events.publish('my-message', { 'city': this.city });
+    this.city = '';
+    // this.navCtrl.push(HomePage, {
+    //   'city': this.city,
+    //   'title': this.title
+    // });
+  }
+
+  showForecastByCoordinatesOnMap(){
+    this.events.publish('my-message', { 'lat': this.lat, 'lng': this.lng });
   }
 
   showKeyboard(){
     this.keyboard.show();
   }
 
-  ionViewDidLoad() {
+  ionViewDidEnter() {
     console.log('ionViewDidLoad SettingsPage');
+    this.initMap();
+  }
+
+  public initMap(): void {
+    console.log("qwe");
+    var mapCanvas = document.getElementById("map");
+    // var marker = new google.maps.Marker();
+    var myCenter = new google.maps.LatLng(48,24);
+    var mapOptions = {
+        center: myCenter,
+        zoom: 8
+    }
+    this.map = new google.maps.Map(mapCanvas, mapOptions);
+    this.map.addListener('click', (e) => {
+      this.lat = e.latLng.lat();
+      this.lng = e.latLng.lng();
+      this.showForecastByCoordinatesOnMap();
+      console.log(e.latLng.lat()+ '_______' + e.latLng.lng());
+    });
   }
 
 }
